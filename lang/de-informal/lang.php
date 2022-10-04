275715b1fd86aff197895c283caec45a144915c1
Vri <vrifox@vrifox.cc>
2022-10-04 15:14:28 +0200

    add de-informal

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,47 @@
+<?php
+
+// style.ini values
+
+$lang['__link__'] = 'Linkfarbe';
+$lang['__text_theme__'] = 'Textfarbe auf Hauptfarbe des Themes';
+$lang['__background_theme__'] = 'Hauptfarbe des Themes';
+$lang['__text_sidebar__'] = 'Seitenleisten-Textfarbe';
+$lang['__background_sidebar__'] = 'Seitenleisten-Hintergrundfarbe';
+$lang['__background_sidebar_hover__'] = 'Seitenleisten-Hintergrundfarbe (hover)';
+$lang['__text_sidebar_caption__'] = 'Titeltextfarbe in der Seitenleisten';
+$lang['__text_sidebar_toc__'] = 'Inhaltsangaben-Textfarbe';
+$lang['__background_sidebar_toc__'] = 'Inhaltsangaben-Hintergrundfarbe';
+$lang['__background_sidebar_toc_hover__'] = 'hervorhebende Inhaltsangaben-Hintergrundfarbe (hover)';
+$lang['__background_sidebar_toc_highlight__'] = 'hervorhebende Inhaltsangaben-Hintergrundfarbe';
+$lang['__text_sidebar_expand1__'] = 'Textfarbe erweiterbarer Inhaltsangabeschaltflächen';
+$lang['__text_sidebar_expand2__'] = 'Textfarbe erweiterbarer Inhaltsangabeschaltflächen ab Ebene 2';
+$lang['__text_pagefooter__'] = 'Seitenfußleisten-Textfarbe';
+$lang['__text_monospace__'] = 'Monospace-Textfarbe';
+$lang['__background_monospace__'] = 'Monospace-Hintergrundfarbe';
+$lang['__border_monospace__'] = 'Monospace-Rahmenfarbe';
+$lang['__background_code__'] = 'Quelltext-Hintergrundfarbe';
+$lang['__border_code__'] = 'Quelltext-Rahmenfarbe';
+$lang['__text_button__'] = 'Schaltflächen-Textfarbe';
+$lang['__background_button__'] = 'Schaltflächen-Hintergrundfarbe';
+$lang['__text_button_hover__'] = 'Textfarbe';
+$lang['__background_button_hover__'] = 'Schaltflächen-Hintergrundfarbe (hover)';
+$lang['__border_edit_highlight__'] = 'hervorhebende Rahmenfarbe des Editors';
+$lang['__background_edit_highlight1__'] = 'hervorhebende Hintergrundfarbe des Editors I';
+$lang['__background_edit_highlight2__'] = 'hervorhebende Hintergrundfarbe des Editors II';
+$lang['__width_site__'] = 'Breite der Seite (nur für Zentrierung)';
+$lang['__width_content__'] = 'Breite des Inhalts';
+$lang['__width_sidebar__'] = 'Breite der Seitenleiste';
+$lang['__background_header__'] = 'Hintergrundfarbe der Kopfzeile';
+$lang['__background_footer__'] = 'Hintergrundfarbe der Fußzeile';
+$lang['__height_header__'] = 'Höhe der Kopfzeile';
+$lang['__height_footer__'] = 'Höhe der Fußzeile';
+$lang['__media_query_sp__'] = 'CSS-Umbruch for Smartphones';
+$lang['__media_query_tb__'] = 'CSS-Umbruch für Tablets';
+
+$lang['breadcrumbs_docs'] = 'Doku';
+$lang['breadcrumbs_admin'] = 'Admin';
+$lang['pageheader_editthispage'] = 'Diese Seite bearbeiten';
+$lang['btn_next'] = 'Nächste';
+$lang['btn_previous'] = 'Vorherige';
+$lang['toc_title'] = 'Inhaltsangabe';
+$lang['js']['searchform_placeholder'] = 'Doku durchsuchen';
